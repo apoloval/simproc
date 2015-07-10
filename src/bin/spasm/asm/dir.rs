f9333c6155f2812156740114b62af4a93e60ce7b
Alvaro Polo <apoloval@gmail.com>
2015-07-10 18:55:20 +0200

    Introduce new directive `DefData`

--- src/bin/spasm/asm/dir.rs
+++ src/bin/spasm/asm/dir.rs
@@ -6,6 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::ascii::AsciiExt;
 use std::fmt::{Display, Error, Formatter};
 
 use asm::assembly::SymbolicAssembly;
@@ -15,10 +16,13 @@ use asm::parser::{Parameterized, parse_num};
 /// Directives are used in assembly language to declare relevant information
 /// about the assembly process, like:
 ///
+/// * `DefData(data_size: u8, bytes: Vec<String>)`: defines a data vector comprised by
+/// elements of _data_size_ bytes.
 /// * `Origin(address: usize)`: indicates the memory address where the following
 /// instructions will be assembled.
 #[derive(Debug, PartialEq)]
 pub enum Directive {
+    DefData(u8, Vec<String>),
     Origin(usize)
 }
 
@@ -48,8 +52,10 @@ impl Directive {
 
     /// Retrieve a directive from the parsed token
     pub fn from_params(par: &Parameterized) -> Result<Directive, DirImportErr> {
-        match par.elem() {
-            "org" | "ORG" => Directive::org_from_params(par),
+        match par.elem().to_ascii_lowercase().trim() {
+            "org" => Directive::org_from_params(par),
+            "db" => Directive::data_from_params(1, par),
+            "dw" => Directive::data_from_params(2, par),
             elem => Err(DirImportErr::Unknown(elem.to_string())),
         }
     }
@@ -65,8 +71,15 @@ impl Directive {
         Err(DirImportErr::BadParams(par.clone()))
     }
 
+    fn data_from_params(dsize: u8, par: &Parameterized) -> Result<Directive, DirImportErr> {
+        let params = par.params();
+        if params.len() == 0 { return Err(DirImportErr::BadParams(par.clone())); }
+        Ok(Directive::DefData(dsize, params.clone()))
+    }
+
     pub fn apply(&self, asm: &mut SymbolicAssembly) {
         match self {
+            &Directive::DefData(_, _) => {},
             &Directive::Origin(ref orig) => asm.ctx_mut().set_addr(*orig),
         }
     }
@@ -91,6 +104,30 @@ mod test {
     fn should_fail_make_org_from_invalid_params() {
         should_fail_make_from_invalid_params(&param!("org"));
         should_fail_make_from_invalid_params(&param!("org", "foobar"));
+    }
+
+    #[test]
+    fn should_make_db_from_params() {
+        assert_eq!(
+            Ok(Directive::DefData(1, vec!("1".to_string(), "2".to_string(), "foo".to_string()))),
+            Directive::from_params(&param!("db", "1", "2", "foo")));
+    }
+
+    #[test]
+    fn should_fail_make_db_from_invalid_params() {
+        should_fail_make_from_invalid_params(&param!("db"));
+    }
+
+    #[test]
+    fn should_make_dw_from_params() {
+        assert_eq!(
+            Ok(Directive::DefData(2, vec!("1".to_string(), "2".to_string(), "foo".to_string()))),
+            Directive::from_params(&param!("dw", "1", "2", "foo")));
+    }
+
+    #[test]
+    fn should_fail_make_dw_from_invalid_params() {
+        should_fail_make_from_invalid_params(&param!("dw"));
     }
 
     #[test]
